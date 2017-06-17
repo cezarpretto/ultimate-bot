5014354f5ea3da8c9767d936d383601c74e83f7c
Cezar Vinícius de O. Pretto <cezar@planosassessoria.com.br>
2017-06-17 16:36:32 -0400

    Retirei algumas coisas do configs

--- modules/configs.js
+++ modules/configs.js
@@ -1,6 +1,5 @@
 const configs = {
-  IP_NCM_HELPER: 'http://172.16.10.79/planilhas/',
-  API_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImNwZiI6IjAzNDc2NTE5MTcxIn0sImlhdCI6MTQ5NzYxMjY0M30.QOx-QPX6uOsjALidQHOlG7_LRXfy8pyUdMvrZyu-fJc'
+  
 }
 
 module.exports = configs
